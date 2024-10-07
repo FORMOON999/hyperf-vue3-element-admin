c02ad21afc980b6e77ef151a8090c8c2dd48fcdd
ice <xykxyk2008@163.com>
2024-10-07 15:14:18 +0800

    加密

--- server/app/Common/Middleware/EncryptMiddleware.php
+++ server/app/Common/Middleware/EncryptMiddleware.php
@@ -12,30 +12,41 @@ declare(strict_types=1);
 namespace App\Common\Middleware;
 
 use App\Common\Core\ServiceTrait;
-use App\Common\Helpers\AseHelper;
+use App\Common\Helpers\AesHelper;
 use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpMessage\Stream\SwooleStream;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use function Hyperf\Support\env;
 
 class EncryptMiddleware implements MiddlewareInterface
 {
     use ServiceTrait;
     #[Inject]
-    protected AseHelper $aseHelper;
+    protected AesHelper $aesHelper;
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+        $status = false;
+        $paths = [
+            "/api/v1/admin",
+        ];
         $response = $handler->handle($request);
-        if (!str_starts_with($request->getUri()->getPath(), "/api") || \Hyperf\Support\env('APP_ENV') != 'prod') {
+        foreach ($paths as $path) {
+            if (str_starts_with($request->getUri()->getPath(), $path)) {
+                $status = true;
+                break;
+            }
+        }
+        if ($status || env('APP_ENV') != 'prod') {
             return $response;
         }
 
         $result = json_decode($response->getBody()->getContents(), true);
         if (!empty($result['data'])) {
-            $result['data'] = $this->aseHelper->encrypt($this->toJson($result['data']));
+            $result['data'] = $this->aesHelper->encrypt($this->toJson($result['data']));
         }
         return $response->withBody(new SwooleStream($this->toJson($result)));
     }
